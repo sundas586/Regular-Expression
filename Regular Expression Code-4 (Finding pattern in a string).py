68e6b6256d0837ae8b62e5d85883f10c02d207ac
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-03 18:53:19 -0700

    Create Regular Expression Code-4 (Finding pattern in a string).py

---
+++ Regular Expression Code-4 (Finding pattern in a string).py
@@ -0,0 +1,6 @@
+import re
+
+txt = 'a abc abcd abcde abcdef abcdefg abcdefgh eeeeee gh bii opop kk plp cxyz uvw'
+line = re.findall("[a-c]+",txt) #Starts with at least one letter between a to c, then one more any letter, then as many letters
+print(line)
+    
